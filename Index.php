57b727bba65b1389eabde2ed1737b5e668d60494
JeannotM <42801349+JeannotM@users.noreply.github.com>
2019-06-26 10:50:02 +0200

    Initital Commit
    Meer opdrachten van PHP

--- Index.php
+++ Index.php
@@ -1,3 +1,23 @@
+<style>
+*{
+    text-align:center;
+    margin:auto;
+    font-family:arial;
+    margin:0;
+}
+</style>
+<h1>simpele HTML</h1>
+<a href="03/Apen.php"><p>Apen</p></a>
+<a href="03/Busreis.php"><p>Busreis</p></a>
+<a href="03/TekenEenKerstboom.php"><p>Kerstboom</p></a>
+<a href="03/VanWhileNaarForLoop.php"><p>Loops</p></a><br>
+<a href="04/Functie.php"><p>Functie</p></a><br>
+<a href="05/Beasties.php"><p>Beasties</p></a>
+<a href="05/DynamischeWebsite"><p>Dynamische Website</p></a>
+<a href="05/loginformulier.php"><p>Login</p></a><br>
+<a href="07/inlog.php"><p>Login 2</p></a>
+<a href="08/aapoverzicht.php"><p>Aap overzicht</p></a>
+<a href="08/radioprogramma.php"><p>Radio Programma</p></a><br>
 <?php
-echo "hey";
-phpinfo();
+$a=24;
+echo "<h2>jksbfbjkb ".$a." jkafjkbad</h2>";
